8df64b621495c3a334d566a3b59c2534d3c27c76
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-05-29 14:09:13 -0500

    Completed Get Users - Added 200 and 500

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,37 @@
-const express = 'express';
+const express = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const userDB = require("./userDb.js");
 
+router.post("/", (req, res) => {});
+
+router.post("/:id/posts", (req, res) => {});
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await userDB.get();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(`:: ERROR IN GET USERS :: ${error}`);
+    res.status(500).json({ ErrorMessage: "Could not retrieve the users." });
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.get("/:id/posts", (req, res) => {});
 
-router.get('/', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
